d25f960218765867cdc96f049abb4444fe6fefc9
Chase1Alpha <111798@gmail.com>
2016-02-11 20:16:09 -0500

    1.1

--- FirstyPy.py
+++ FirstyPy.py
@@ -1,5 +1,13 @@
+import urllib
 import urllib2
+from bs4 import BeautifulSoup
 
-request = urllib2.Request("http://www.baidu.com")
-response = urllib2.urlopen(request)
+url = 'http://www.ttmeiju.com/meiju/The.Big.Bang.Theory.html'
+#user_agent = 'Mozilla/4.0(compatible; MSIE 5.5; Windows NT)'
+#headers = {'User-Agent' : user_agent}
+request = urllib2.Request(url)#, headers)
+try:
+    response = urllib2.urlopen(request)
+except urllib2.URLError, e:
+    print e.reason
 print response.read()

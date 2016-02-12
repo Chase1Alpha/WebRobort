e48443b1cfdd6e5a5ad078807d9bd090bac98351
Chase1Alpha <111798@gmail.com>
2016-02-11 21:55:52 -0500

    1.2

--- FirstyPy.py
+++ FirstyPy.py
@@ -1,13 +1,24 @@
 import urllib
 import urllib2
 from bs4 import BeautifulSoup
+import re
 
 url = 'http://www.ttmeiju.com/meiju/The.Big.Bang.Theory.html'
-#user_agent = 'Mozilla/4.0(compatible; MSIE 5.5; Windows NT)'
-#headers = {'User-Agent' : user_agent}
-request = urllib2.Request(url)#, headers)
+
+request = urllib2.Request(url)
+
 try:
     response = urllib2.urlopen(request)
 except urllib2.URLError, e:
     print e.reason
-print response.read()
+
+soup = BeautifulSoup(response)
+
+#print soup.html.find_all(href=re.compile("magnet"))
+
+download = soup.html.find_all(href=re.compile("magnet"))
+
+def saveBrief(download):
+    fileName = "download.txt"
+    f = open(fileName, 'w+')
+    f.write(download.encode('utf-8'))
